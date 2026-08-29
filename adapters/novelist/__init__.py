b091a15fac31e931c697d4c7c51747eace3c9a48
the static collective <joyvolcanomusic@gmail.com>
2026-08-29 00:03:30 -0500

    feat: export mortal narrative evaluator

--- adapters/novelist/__init__.py
+++ adapters/novelist/__init__.py
@@ -1,6 +1,9 @@
+from .evaluator import NarrativeInputError, evaluate_beat_proposal
 from .formation import projection_formation_refs, validate_formation_receipt
 
 __all__ = [
+    "NarrativeInputError",
+    "evaluate_beat_proposal",
     "projection_formation_refs",
     "validate_formation_receipt",
 ]
