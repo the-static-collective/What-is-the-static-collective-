6dcbd6cfaf9a976e63b51dad00158746db9f626c
the static collective <joyvolcanomusic@gmail.com>
2026-08-29 00:05:08 -0500

    feat: export mortal narrative render guard

--- adapters/novelist/__init__.py
+++ adapters/novelist/__init__.py
@@ -1,8 +1,10 @@
 from .evaluator import NarrativeInputError, evaluate_beat_proposal
 from .formation import projection_formation_refs, validate_formation_receipt
+from .render_guard import check_render_receipt
 
 __all__ = [
     "NarrativeInputError",
+    "check_render_receipt",
     "evaluate_beat_proposal",
     "projection_formation_refs",
     "validate_formation_receipt",
